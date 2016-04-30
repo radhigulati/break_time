9db153aba2fe590859d2793e090e2e159657f4fc
Radhika Gulati <radhigulati@gmail.com>
2016-04-30 15:46:52 -0700

    added import time

--- break_time.py
+++ break_time.py
@@ -1,3 +1,10 @@
 import webbrowser
+import time
 
-webbrowser.open("https://www.youtube.com/watch?v=uB1D9wWxd2w")
+total_breaks = 3
+break_count = 0
+
+while (break_count < total_breaks)
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=uB1D9wWxd2w")
+    break_count +=1
